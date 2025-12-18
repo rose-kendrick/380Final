3e475a82d89a286a4550b352b194bad6afc3f3dc
R K <rk@ip-170-38.emich.edu>
2025-12-17 22:16:33 -0500

    Made threebody.py a true three-body problem where the sun is not fixed. I have then played around with the mass of jupiter to see what that does to the model.

--- threebody.py
+++ threebody.py
@@ -1,82 +1,115 @@
 import numpy as np 
 from matplotlib import pyplot as pp 
 
-def FgS(m1,m2,r_pos,r):
-    F = (-G*m1*m2*r_pos)/(r**3)
+
+def F(m1,m2,r,dx):
+    F = (-G*m1*m2*(dx))/((r**3))
     return F
 
-def Fej(mj,me,r,x1,x2):
-    F = (-G*mj*me*(x2-x1))/(np.abs(r**3))
-    return F
-
-def dvdt(F1,F2,m2):
-    dvxdt = (F1+F2)/m2
+def dvdt(F1,F2,m):
+    dvxdt = (F1+F2)/m
     return dvxdt
 
 def r(dx,dy):
     r = np.sqrt((dx)**2+(dy)**2)
     return r
 
-G = 4*np.pi**2 # Gravitational constant (AU^3/(mS*year^2))
-mE = 3.0027e-6 # Mass of Earth (Solar Mass)
-mS = 1 # Mass of Sun (Solar Mass)
-mJ = 0.000954588*1000 # Mass of jupiter (Solar Mass)
-dt = 0.001 # Time step (years)
-tFinal = 12 # End time (years)
-N = int((tFinal+dt)/dt) # Number of time steps
-sunx = 0 # Sun x-position (AU)
-suny = 0 # Sun y-position (AU)
 
+'''TIME'''
+dt = 0.0001 # Time step (years)
+tFinal = 40 # End time (years)
+N = int((tFinal+dt)/dt) # Number of time steps
 time = np.linspace(0,tFinal+dt,N)
+
+
+'''EARTH initial conditions'''
+mE = 3.0027e-6 # Mass of Earth (Solar Mass)
 
 xE = np.zeros(N)
 yE = np.zeros(N)
 vEx = np.zeros(N)
 vEy = np.zeros(N)
 
-xE[0] = 1 # Earth initial x-position (AU)
+xE[0] = 0.9950378913690774 # Earth initial x-position (AU)
 yE[0] = 0 # Earth initial y-position (AU)
 vEx[0] = 0 # Earth initial x-velocity (AU/year)
 vEy[0] = 2*np.pi # Earth initial y-velocity (AU/year)
+pEy = 1.886652e-5 #Earth initial y-momentum (Solar Mass*AU/year) - Used to find v0y-sun
 
+'''JUPITER initial conditions'''
+mJ = 0.000954588*1000 # Mass of jupiter (Solar Mass)
 xJ = np.zeros(N)
 yJ = np.zeros(N)
 vJx = np.zeros(N)
 vJy = np.zeros(N)
 
-xJ[0] = 5.2 # Jupiter initial x-position (AU)
+xJ[0] = 5.195037891369077 # Jupiter initial x-position (AU)
 yJ[0] = 0 # Jupiter initial y-position (AU)
 vJx[0] = 0 # Jupiter initial x-velocity (AU/year)
 vJy[0] = 2*np.pi*xJ[0]/12 # Jupiter initial y-velocity (AU/year)
+pJy = 0.0025990698 #Jupiter initial y-momentum (Solar Mass*AU/year) - Used to find v0y-sun
 
+
+'''SUN initial conditions'''
+mS = 1 # Mass of Sun (Solar Mass)
+
+xS = np.zeros(N)
+yS = np.zeros(N)
+vSx = np.zeros(N)
+vSy = np.zeros(N)
+
+xS[0] = -0.004962108630922639 # Sun initial x-position (AU)
+yS[0] = 0 # Sun initial y-position (AU)
+vSx[0] = 0 # Sun initial x-velocity (AU/year)
+vSy[0] = -0.0026179363 # Sun initial y-velocity (AU/year)
+pSy = -0.0026179363 #Sun initial y-momentum (Solar Mass*AU/year) - Used to find v0y-sun
+
+
+Xcm = (xS[0]*mS+xE[0]*mE+xJ[0]*mJ)/(mS+mE+mJ)
+G = 4*np.pi**2 # Gravitational constant (AU^3/(mS*year^2))
 
 for i in range(N-1):
-
-    dxES = xE[i]-sunx
-    dyES = yE[i]-suny
+    '''Earth'''
+    dxES = xE[i]-xS[i]
+    dyES = yE[i]-yS[i]
     dxEJ = xE[i]-xJ[i]
     dyEJ = yE[i]-yJ[i]
 
-    FgSunEx = FgS(mS,mE,dxES,r(dxES,dyES))
-    FEarthJupiterx = Fej(mJ,mE,r(dxEJ,dyEJ),xE[i],xJ[i])
-    aEx = dvdt(FgSunEx,FEarthJupiterx,mE)
+    FSunEx = F(mE,mS,r(dxES,dyES),dxES)
+    FJupiterEx = F(mE,mJ,r(dxEJ,dyEJ),dxEJ)
+    aEx = dvdt(FSunEx,FJupiterEx,mE)
 
-    FgSunEy = FgS(mS,mE,dyES,r(dxES,dyES))
-    FEarthJupitery = Fej(mJ,mE,r(dxEJ,dyEJ),yE[i],yJ[i])
-    aEy = dvdt(FgSunEy,FEarthJupitery,mE)
+    FSunEy = F(mE,mS,r(dxES,dyES),dyES)
+    FJupiterEy = F(mE,mJ,r(dxEJ,dyEJ),dyEJ)
+    aEy = dvdt(FSunEy,FJupiterEy,mE)
 
-    dxJS = xJ[i]-sunx
-    dyJS = yJ[i]-suny
+    '''Jupiter'''
+    dxJS = xJ[i]-xS[i]
+    dyJS = yJ[i]-yS[i]
     dxJE = xJ[i]-xE[i]
     dyJE = yJ[i]-yE[i]
 
-    FgSunJx = FgS(mS,mJ,dxJS,r(dxJS,dyJS))
-    FJupiterEarthx =Fej(mJ,mE,r(dxJE,dyJE),xJ[i],xE[i])
-    aJx = dvdt(FgSunJx,FJupiterEarthx,mJ)
+    FSunJx = F(mJ,mS,r(dxJS,dyJS),dxJS)
+    FEarthJx =F(mJ,mE,r(dxJE,dyJE),dxJE)
+    aJx = dvdt(FSunJx,FEarthJx,mJ)
 
-    FgSunJy = FgS(mS,mJ,dyJS,r(dxJS,dyJS))
-    FJupiterEarthy = Fej(mJ,mE,r(dxJE,dyJE),yJ[i],yE[i])
-    aJy = dvdt(FgSunJy,FJupiterEarthy,mJ)
+    FSunJy = F(mJ,mS,r(dxJS,dyJS),dyJS)
+    FEarthJy =F(mJ,mE,r(dxJE,dyJE),dyJE)
+    aJy = dvdt(FSunJy,FEarthJy,mJ)
+
+    '''Sun'''
+    dxSJ = xS[i]-xJ[i]
+    dySJ = yS[i]-yJ[i]
+    dxSE = xS[i]-xE[i]
+    dySE = yS[i]-yE[i]
+
+    FEarthSx = F(mS,mE,r(dxSE,dySE),dxSJ)
+    FJupiterSunx =F(mS,mJ,r(dxSJ,dySJ),dySE)
+    aSx = dvdt(FEarthSx,FJupiterSunx,mS)
+
+    FEarthSy = F(mS,mE,r(dxSE,dySE),dySJ)
+    FJupiterSuny = F(mS,mJ,r(dxSJ,dySJ),dySE)
+    aSy = dvdt(FEarthSy,FJupiterSuny,mS)
 
     vEx[i+1] = vEx[i]+aEx*dt
     vEy[i+1] = vEy[i]+aEy*dt
@@ -84,34 +117,42 @@ for i in range(N-1):
     vJx[i+1] = vJx[i]+aJx*dt
     vJy[i+1] = vJy[i]+aJy*dt
 
+    vSx[i+1] = vSx[i]+aSx*dt
+    vSy[i+1] = vSy[i]+aSy*dt
+
     xE[i+1] = xE[i]+vEx[i+1]*dt
     yE[i+1] = yE[i]+vEy[i+1]*dt
 
     xJ[i+1] = xJ[i]+vJx[i+1]*dt
     yJ[i+1] = yJ[i]+vJy[i+1]*dt
+
+    xS[i+1] = xS[i]+vSx[i+1]*dt
+    yS[i+1] = yS[i]+vSy[i+1]*dt
     
 
 pp.figure()
 pp.plot(xE,yE,label='Earth',color='blue')
 pp.plot(xJ,yJ,label='Jupiter',color='red')
-pp.scatter(sunx,suny,color='orange',label='Sun')
+pp.plot(xS,yS,label='Sun',color='orange')
 pp.xlabel("x (AU)")
 pp.ylabel("y (AU)")
 pp.title("Position")
-pp.legend(loc='upper right')
+pp.legend(loc='lower right')
 pp.axis("equal")
-pp.show()
-# pp.savefig('pos3bdyR1000MJ.png')
+# pp.show()
+pp.savefig('FULLpos3bdy1000MJdt0.0001.png')
 
 pp.figure()
 pp.plot(time,xE,label="xEarth(t)",color='green')
 pp.plot(time,yE,label="yEarth(t)",color='blue')
 pp.plot(time,xJ,label='xJupiter(t)',color='red')
 pp.plot(time,yJ,label='yJupiter(t)',color='orange')
+pp.plot(time,xS,label='xSun(t)',color='purple')
+pp.plot(time,yS,label='ySun(t)',color='yellow')
 pp.xlabel('Time (years)')
 pp.ylabel('Position (AU)')
 pp.title('x and y-Position vs. Time')
-pp.legend(loc="upper right")
-pp.show()
-# pp.savefig('posvt3bdyR1000MJ.png')
+pp.legend(loc="lower left")
+# pp.show()
+pp.savefig('FULLposvt3bdy1000MJdt0.0001.png')
 
